c9a51cfb12940b599821131d0dbc19c7349b80aa
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-12-09 22:19:05 +0100

    📦 NEW: Add avatar setting feature

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const User = require("../../models/schemaUser");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const auth = require("../../auth/auth");
+const gravatar = require("gravatar");
+const { updateAvatar } = require("../../utils/avatar");
+const { uploadAvatar } = require("../../utils/multer");
 
 const secret = process.env.AUTH_SECRET;
 
@@ -18,12 +21,15 @@ router.post("/signup", async (req, res, next) => {
     }
 
     try {
-        const newUser = new User({ email });
+        const newUser = new User({
+            email,
+            avatarURL: gravatar.url(email, { s: "200", r: "pg", d: "retro" }),
+        });
         await newUser.setPassword(password);
         await newUser.save();
-        return res
-            .status(201)
-            .json({ message: `Registration successful: user ${email}` });
+        return res.status(201).json({
+            message: `Registration successful: user ${email}, avatar ${newUser.avatarURL}`,
+        });
     } catch (error) {
         next(error);
     }
@@ -62,12 +68,13 @@ router.post("/login", async (req, res, next) => {
 
 router.get("/logout", auth, async (req, res, next) => {
     try {
-        const user = req.user;
+        const user = await User.findById(req.user._id);
         if (!user) {
             return res.status(401).json({ message: "User not authorized!" });
         }
         user.token = null;
         await user.save();
+
         return res.status(200).json({ message: "Logout successful" });
     } catch (error) {
         next(error);
@@ -76,8 +83,7 @@ router.get("/logout", auth, async (req, res, next) => {
 
 router.get("/current", auth, async (req, res, next) => {
     try {
-        const user = req.user;
-
+        const user = await User.findById(req.user._id);
         if (!user) {
             return res.status(401).json({ message: "User not authorized!" });
         }
@@ -88,5 +94,16 @@ router.get("/current", auth, async (req, res, next) => {
         next(error);
     }
 });
+
+router.patch(
+    "/avatars",
+    uploadAvatar.single("avatar"),
+    (req, res, next) => {
+        console.log("File received:", req.file);
+        console.log("Body data:", req.body);
+        next();
+    },
+    updateAvatar
+);
 
 module.exports = router;
