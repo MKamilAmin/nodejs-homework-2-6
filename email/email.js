c724517bcb7ef51e5057df5dcf9b42c6d090147b
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-12-17 00:14:03 +0100

    📦 NEW: Add email functionality

---
+++ email/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MAIL_USER, MAIL_PASS } = process.env;
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.mailgun.org",
+    port: 587,
+    secure: false,
+    auth: {
+        type: "login",
+        user: MAIL_USER,
+        pass: MAIL_PASS,
+    },
+});
+
+const emailSender = async (to, subject, html) => {
+    await transporter.sendMail({
+        from: '"Mojo company" <mojo.company@gmail.com>',
+        to,
+        subject,
+        html,
+    });
+    console.log("Message sent");
+};
+
+module.exports = { emailSender };
